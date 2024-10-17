008baa71ad8ea9f6592156eecb86f27dc7b1f645
Jocilé Moreira Serra <jocile@gmail.com>
2024-10-17 15:44:34 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,25 @@
+"""
+Calculates the arithmetic mean of four grades provided by the user.
+
+Args:
+    nota1 (float): The first grade.
+    nota2 (float): The second grade.
+    nota3 (float): The third grade.
+    nota4 (float): The fourth grade.
+
+Returns:
+    float: The arithmetic mean of the four grades.
+"""
+# coding: utf-8
+# Construa um algoritmo que calcule a média aritmética entre quatro notas quaisquer fornecidas pelo usuário.
+# def media(nota1, nota2, nota3, nota4):
+#    return (nota1 + nota2 + nota3 + nota4) / 4
+
+#print("A média é: ", media(10, 8, 9, 7))
+
+nota1 = 10
+nota2 = 8
+nota3 = 9
+nota4 = 7
+media = (nota1 + nota2 + nota3 + nota4)/4
+print(f'A media e: {media}')
